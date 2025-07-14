05e414bbbf2a35c1d06e3eacbb022be2f3c40c48
SIAM632713 <siamrezwanahmed@gmail.com>
2025-07-15 00:55:00 +0600

    g7

--- src/middleware/verifyToken.js
+++ src/middleware/verifyToken.js
@@ -4,10 +4,22 @@ import {JWT_SECRET} from "../../index.js";
 
 export const verifyToken = (req, res, next) => {
     try {
-        const token=req.cookies.token;
-        if(!token){
-            return res.status(400).send({message:"Token not found"})
+
+        let token ;
+
+        const authHeader=req.headers.authorization;
+        if(authHeader && authHeader.startsWith('Bearer ')){
+            token=authHeader.split('')[1];
         }
+
+        if(!token && req.cookies.token){
+            token=req.cookies.token;
+        }
+
+        if(!token){
+            return res.status(401).json({message:"Unauthorized, Access denied!"});
+        }
+
         const decoded=jwt.verify(token,JWT_SECRET)
         if(!decoded.userID){
             return res.status(400).send({message:"Token not verified"})
